faa86433bfb6b98579d064286049b46402a3f88e
grjctin <93117447+grjctin@users.noreply.github.com>
2023-05-16 19:28:28 +0300

    End of section 12

--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -1,6 +1,8 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { AccountService } from '../_services/account.service';
 import { ToastrService } from 'ngx-toastr';
+import { AbstractControl, FormControl, FormGroup, ValidatorFn, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
@@ -10,26 +12,68 @@ import { ToastrService } from 'ngx-toastr';
 export class RegisterComponent{
   //@Input() usersFromHomeComponent: any;
   @Output() cancelRegister = new EventEmitter();
-  model: any = {} //empty object
+  //model: any = {} era pentru template forms
+  registerForm: FormGroup = new FormGroup({}); //pentru reactive forms
+  maxDate: Date = new Date();
+  validationErrors: string[] | undefined;
 
 
-  constructor(private accountService: AccountService, private toastr: ToastrService) { }
+  constructor(private accountService: AccountService,
+     private toastr: ToastrService, 
+     private router: Router) { }
 
   ngOnInit(): void {
+    this.initializeForm();
+    this.maxDate.setFullYear(this.maxDate.getFullYear() -18);
+  }
+
+  initializeForm() {
+    this.registerForm = new FormGroup({
+      gender: new FormControl('male'),
+      username: new FormControl('', Validators.required),
+      knownAs: new FormControl('', Validators.required),
+      dateOfBirth: new FormControl('', Validators.required),
+      city: new FormControl('', Validators.required),
+      country: new FormControl('', Validators.required),
+      password: new FormControl('', [Validators.required, 
+        Validators.minLength(4),Validators.maxLength(8)]),
+      confirmPassword: new FormControl('', [Validators.required, this.matchValues('password')])
+    });
+    this.registerForm.controls['password'].valueChanges.subscribe({
+      next: () => this.registerForm.controls['confirmPassword'].updateValueAndValidity()
+    })
+  }
+
+  //validator pentru a compara parolele introduse in form
+  matchValues(matchTo: string): ValidatorFn {
+    return(control: AbstractControl) => {
+      return control.value === control.parent?.get(matchTo)?.value ? null : {notMatching: true}
+    }
   }
 
   register() {
-    this.accountService.register(this.model).subscribe({
-      next: () => this.cancel,
+    const dob = this.getDateOnly(this.registerForm.controls['dateOfBirth'].value);
+    const values = {...this.registerForm.value,dateOfBirth: dob}
+    this.accountService.register(values).subscribe({
+      next: () => {
+        this.router.navigateByUrl('/members')
+      },
       error: error => {
-        this.toastr.error(error.error);
-        console.log(error);
+        this.validationErrors = error
       }
     })
   }
 
   cancel() {
     this.cancelRegister.emit(false);
+  }
+
+  //metoda pentru a returna doar data, nu si timpul si timezoneul
+  private getDateOnly(dob: string | undefined) {
+    if(!dob) return;
+    let theDob = new Date(dob);
+    return new Date(theDob.setMinutes(theDob.getMinutes()-theDob.getTimezoneOffset()))
+    .toISOString().slice(0,10);
   }
 
 }
